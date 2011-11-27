e16c42f99a2b9120d0ccadcd7bf493a03cc59cec
Roberto Rodriguez <roberto@rodriguezartavia.com>
2011-11-27 12:40:00 -0600

    add allow origin change

--- index.js
+++ index.js
@@ -6,7 +6,17 @@ var resolve  = require('path').resolve;
 var path = resolve(argv._[0] || process.cwd());
 var port = argv.p || argv.port || process.env.PORT || 9294;
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
 server = express.createServer();
+server.use(allowCrossDomain);
+
 
 server.get('/cache.manifest', function(req, res){
     res.header('Content-Type', 'text/cache-manifest');
